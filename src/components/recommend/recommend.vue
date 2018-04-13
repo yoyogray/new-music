16a6cc3b6def8e13d6ff27a8ef1c7570318abe87
yuegray <474851714@qq.com>
2018-04-13 16:32:25 +0800

    get picture info

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="recommend">
     <div class="recommend-content">
-      <div class="slider-wrapper"></div>
+      <div class="slider-wrapper">
+        <slider>
+          <div v-for="item in recommends ">
+            <a :href="item.linkUrl">
+              <img :src="item.picUrl" alt="">
+            </a>
+          </div>
+        </slider>
+      </div>
       <div class="recommend-list">
         <h1 class="list-title">热门歌单推荐</h1>
         <ul></ul>
@@ -13,8 +21,14 @@
 <script type="text/ecmascript-6">
   import {getRecommend} from 'api/recommend'
   import {ERR_OK} from 'api/config'
+  import Slider from 'base/slider/slider'
 
   export default {
+    data() {
+      return {
+        recommends: []
+      }
+    },
     created() {
       this._getRecommend()
     },
@@ -23,9 +37,13 @@
         getRecommend().then((res) => {
           if (res.code === ERR_OK) {
             console.log(res.data.slider)
+            this.recommends = res.data.slider
           }
         })
       }
+    },
+    components: {
+      Slider
     }
   }
 </script>
