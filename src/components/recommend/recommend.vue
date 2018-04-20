558b5cbfd402155bbf5ca6e13a0bd89819b4d798
yuegray <474851714@qq.com>
2018-04-20 11:14:13 +0800

    拼写错误的mounted导致

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="recommend">
     <div class="recommend-content">
-      <div class="slider-wrapper">
+      <div v-if="recommends.length" class="slider-wrapper">
         <slider>
           <div v-for="item in recommends ">
             <a :href="item.linkUrl">
@@ -14,7 +14,7 @@
         <h1 class="list-title">热门歌单推荐</h1>
         <ul></ul>
       </div>
-    </div>
+    </div>  
   </div>
 </template>
 
@@ -22,7 +22,7 @@
   import {getRecommend} from 'api/recommend'
   import {ERR_OK} from 'api/config'
   import Slider from 'base/slider/slider'
-
+  
   export default {
     data() {
       return {
